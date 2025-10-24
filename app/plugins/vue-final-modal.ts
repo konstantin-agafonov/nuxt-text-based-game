33d6d288957d8b74bbf441ebf1ecfd05bee8f9b4
Konstantin Agafonov <konstantin.agafonov@gmail.com>
2025-10-24 22:09:16 +0300

    vue-final-modal fix

--- app/plugins/vue-final-modal.ts
+++ app/plugins/vue-final-modal.ts
@@ -1,7 +1,9 @@
-import { createVfm } from 'vue-final-modal'
+import { createVfm, VueFinalModal, ModalsContainer } from 'vue-final-modal'
+import 'vue-final-modal/style.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
-    const vfm = createVfm() as any
-
+    const vfm = createVfm()
     nuxtApp.vueApp.use(vfm)
+    nuxtApp.vueApp.component('VueFinalModal', VueFinalModal)
+    nuxtApp.vueApp.component('ModalsContainer', ModalsContainer)
 })
