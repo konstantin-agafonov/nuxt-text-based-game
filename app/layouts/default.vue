7ce2511d3a1c445ec7f691866cbe226032c2c651
Konstantin Agafonov <konstantin.agafonov@gmail.com>
2025-10-25 20:18:28 +0300

    fix layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,17 +3,20 @@
 </script>
 
 <template>
-  <Header/>
+  <div class="h-screen flex flex-col overflow-hidden">
+    <Header/>
 
-  <div class="flex justify-stretch">
-    <Aside/>
+    <div class="flex flex-1 overflow-hidden">
+      <Aside/>
 
-    <main class="min-h-screen bg-gray-50 px-4 sm:px-6 lg:px-8 py-8">
-      <slot/>
-    </main>
+      <main class="flex-1 bg-gray-50 px-4 sm:px-6 lg:px-8 py-8 overflow-y-auto">
+        <slot/>
+      </main>
+    </div>
+
+    <Footer/>
   </div>
-
-  <Footer/>
+  
   <NuxtNotifications position="bottom left" :speed="500" />
   <ModalsContainer />
 </template>
