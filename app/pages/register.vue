66456ea0a86839ad884f8ef096ac0cf1b10234d7
Konstantin Agafonov <konstantin.agafonov@gmail.com>
2025-10-30 21:53:52 +0300

    register form

--- app/pages/register.vue
+++ app/pages/register.vue
@@ -4,9 +4,10 @@ definePageMeta({
 });
 
 interface Credentials {
+  name: string;
   email: string;
   password: string;
-  remember: boolean;
+  password_confirmation: string;
 }
 
 const { login } = useSanctumAuth();
@@ -22,7 +23,7 @@ const error = ref<string>("");
 async function submit() {
   try {
     error.value = "";
-    await login(credentials);
+    await register(credentials);
   } catch (err) {
     error.value = err as string;
   }
@@ -36,28 +37,56 @@ async function submit() {
     <form @submit.prevent="submit">
       <small>{{ error }}</small>
 
-      <input
-          id="username"
-          v-model="credentials.email"
-          type="text"
-          name="username"
-          placeholder="Your username"
-          autocomplete="off"
-      />
-      <input
-          id="password"
-          v-model="credentials.password"
-          type="password"
-          name="password"
-          placeholder="Your password"
-          autocomplete="off"
-      />
+      <div>
+        <label for="username">User name</label>
+        <input
+            id="username"
+            v-model="credentials.name"
+            type="text"
+            name="username"
+            placeholder="Your username"
+            autocomplete="off"
+        />
+      </div>
+      <div>
+        <label for="useremail">User email</label>
+        <input
+            id="useremail"
+            v-model="credentials.email"
+            type="email"
+            name="useremail"
+            placeholder="Your email"
+            autocomplete="off"
+        />
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input
+            id="password"
+            v-model="credentials.password"
+            type="password"
+            name="password"
+            placeholder="Your password"
+            autocomplete="off"
+        />
+      </div>
+      <div>
+        <label for="password_confirmation">Repeat password</label>
+        <input
+            id="password_confirmation"
+            v-model="credentials.password_confirmation"
+            type="password"
+            name="password_confirmation"
+            placeholder="Confirm password"
+            autocomplete="off"
+        />
+      </div>
 
-      <button type="submit">Login</button>
+      <button type="submit">Register</button>
     </form>
 
-    <NuxtLink to="/register" class="text-blue-500">
-      Register
+    <NuxtLink to="/login" class="text-blue-500">
+      Login
     </NuxtLink>
 
     <NuxtLink to="/password-reset" class="text-blue-500">
